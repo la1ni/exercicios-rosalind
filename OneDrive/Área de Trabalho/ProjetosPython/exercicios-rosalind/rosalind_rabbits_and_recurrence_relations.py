88fcee1c9b6f84e44efa7654bb01c3948e7fd338
Mariana Lainara Silva <121519776+la1ni@users.noreply.github.com>
2024-06-04 09:01:35 -0300

    Deleted directory

--- OneDrive/Área de Trabalho/ProjetosPython/exercicios-rosalind/rosalind_rabbits_and_recurrence_relations.py
+++
@@ -1,9 +0,0 @@
-n = 33  # num de meses decorridos
-k = 4  # quantidade de pares de coelhos gerados a cada mês
-coelhos_r = [0]  # coelhos em idade reprodutiva
-coelhos_nr = [1]  # coelhos filhotes
-for c in range(1, n):
-    coelhos_r.append((coelhos_nr[c-1])+(coelhos_r[c-1]))
-    coelhos_nr.append((coelhos_r[c-1]*k))
-soma = (coelhos_nr[n-1]+coelhos_r[n-1])
-print(soma)
